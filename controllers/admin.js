1393334a9004b1f9bc6694e83e6332fdf21f2950
Luke Mueller <muelll79@hotmail.com>
2019-12-04 14:30:50 -0700

    adds insert functionality to mysql from admin/add-product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,9 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
+  product.save()
+    .then(() => redirect('/'))
+    .catch(err => console.log(err));
   res.redirect('/');
 };
 
