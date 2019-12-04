213a217441311674260c608b8c087bdb1c824dc4
Luke Mueller <muelll79@hotmail.com>
2019-12-04 14:41:45 -0700

    fetches single result from database

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,11 +76,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      pageTitle: product.title,
-      path: '/products',
-      product: product
-    });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        pageTitle: product.title,
+        path: '/products',
+        product: product[0]
+      });
+    })
+    .catch(err => console.log(err));
 }
